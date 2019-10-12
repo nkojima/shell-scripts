446e1dd455f462607849806c4d26beaef455478a
nkojima <kojiro629@gmail.com>
2019-10-12 15:25:26 +0900

    Add comfirmation script for password expiration date.

---
+++ pw_expiration.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ログイン可能なユーザーを全て取得して、
+# 各ユーザーのパスワード有効期限を一覧表示する。
+# https://qiita.com/nkojima/items/502119f460017e9e7c1f
+
+users=$(cat /etc/passwd | grep ':/bin/bash$' | sed s/:.*//)
+​
+for user in ${users[@]}; do
+​
+    expiration=$(chage -l $user | sed -n 2p | sed s/.*://)
+    echo $user,$expiration
+​
+done
