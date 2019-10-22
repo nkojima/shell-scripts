156675229bfa32b219c40d112bcd359def649d26
nkojima <kojiro629@gmail.com>
2019-10-22 19:36:07 +0900

    Add hardware_info.sh.

---
+++ hardware_info.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "[CPU Info]"
+echo $(cat /proc/cpuinfo | grep "model name" | sort -u)
+echo $(cat /proc/cpuinfo | grep "cpu cores" | sort -u)
+echo "cpu threads : "$(cat /proc/cpuinfo | grep "processor" | wc -l)
+
+echo "[Memory Info]"
+echo $(cat /proc/meminfo | grep "MemTotal" | sort -u)
+
+echo "[SCSI device]"
+echo $(cat /proc/scsi/scsi)
+
+echo "[PCI-E device]"
+echo $(lspci | grep SSD)
