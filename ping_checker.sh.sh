5848e28bb73d66ca958a83920929183f9084707c
nkojima <kojiro629@gmail.com>
2019-10-25 22:53:32 +0900

    Add "ping_checker.sh".

---
+++ ping_checker.sh.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#!/bin/bash
+​
+IP_LIST=(172.17.12.143 172.17.11.130 172.17.15.81 172.17.15.82)
+MAILTO='your_address@sample.com'
+LOG_FILE=./ping.log
+​
+for ip in ${IP_LIST[@]}
+do
+  ping_result=`ping -w 5 $ip | grep '100% packet loss'`
+  date_result=`date`
+#  echo $ping_result
+​
+  if [[ -n $ping_result ]]; then
+    echo "[SEVERE] server inactive: $ip $date_result" >> $LOG_FILE
+    echo $ip | mail -s "[ALERT] server down!! $date_result" $MAILTO
+  else
+    echo "[INFO] server active: $ip $date_result" >> $LOG_FILE
+  fi
+done
