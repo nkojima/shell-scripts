e314ea165c6f73e3bff7cff7a6db1198af2f1d1e
kojiro629 <kojiro629@gmail.com>
2020-07-16 22:51:46 +0900

    Add VGA-info to hardware_info.sh. #7

--- hardware_info.sh
+++ hardware_info.sh
@@ -11,7 +11,10 @@ echo $(cat /proc/meminfo | grep "MemTotal" | sort -u)
 echo "[SCSI device]"
 echo $(cat /proc/scsi/scsi)
 
-echo "[PCI-E device]"
+echo "[PCI-E device(VGA)]"
+echo "$(lspci | grep -i vga)"
+
+echo "[PCI-E device(SSD)]"
 echo "$(lspci | grep -iE "SSD|Non-Volatile memory")"
 
 echo "[BIOS]"
