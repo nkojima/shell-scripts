8f2e3c931c9b56b11be61eb8966ae64c0fe92055
kojiro629 <kojiro629@gmail.com>
2020-07-18 23:05:24 +0900

    Add 'cpu_stress_test.sh'. #6

---
+++ cpu_stress_test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# CPUの論理コア数（スレッド数）を加味して、CPUのストレステストを実行する。
+# https://i-think-it.net/linux-cpu-stress-how-to/
+
+function usage {
+    cat <<EOS
+$(basename ${0}) is a cpu stress test tool for multi-core processor.
+
+Usage:
+    $(basename ${0}) [<options>]
+
+Options:
+    -n    using n cores in stress test.
+EOS
+}
+
+CPU_THREADS=`grep processor /proc/cpuinfo | wc -l`
+
+while getopts n:h OPT
+do
+  case $OPT in
+    "n" ) CPU_THREADS="$OPTARG"
+          ;;
+    "h" ) usage;
+          exit 0
+          ;;
+  esac
+done
+
+echo $CPU_THREADS
+
+openssl speed -multi $CPU_THREADS
