3a6a076cbfb728101cd877f0dc7995b7728a11d5
kojiro629 <kojiro629@gmail.com>
2020-08-20 22:49:14 +0900

    Update comments of 'cpu_stress_test.sh'.

--- cpu_stress_test.sh
+++ cpu_stress_test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+#-------------------------------------------------------------------------------
 # CPUの論理コア数（スレッド数）を加味して、CPUのストレステストを実行する。
 # https://i-think-it.net/linux-cpu-stress-how-to/
-
+#-------------------------------------------------------------------------------
 function usage {
     cat <<EOS
 $(basename ${0}) is a cpu stress test tool for multi-core processor.
@@ -15,8 +16,10 @@ Options:
 EOS
 }
 
+# CPUの論理コア数を取得する。
 CPU_THREADS=`grep processor /proc/cpuinfo | wc -l`
 
+# パラメータに応じて処理を振り分ける。
 while getopts n:h OPT
 do
   case $OPT in
@@ -28,5 +31,6 @@ do
   esac
 done
 
+# CPUのストレステストを実行する。
 echo "using "$CPU_THREADS" cores in stress test."
 openssl speed -multi $CPU_THREADS > /dev/null 2>&1
