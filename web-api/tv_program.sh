6965ef15119c16370806af0c738165efd6ddf001
kojiro629 <kojiro629@gmail.com>
2020-08-22 19:27:06 +0900

    Add 'tv_program.sh'. #5

--- web-api/tv_program.sh
+++ web-api/tv_program.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-# https://api.nhk.or.jp/v2/pg/list/130/g1/2020-08-01.json?key={YOUR_API_KEY}
 
+#-------------------------------------------------------------------------------
 # NHK番組表APIを利用して、現在放送中の番組を取得するスクリプト。
+#-------------------------------------------------------------------------------
+json=$(curl -s http://api.nhk.or.jp/v2/pg/now/130/g1.json?key={YOUR_API_KEY})
+
+echo '[現在放送中の番組]'
+echo 'チャンネル：'$(echo ${json} | jq '.nowonair_list.g1.present.service.name' | sed 's/\"//g')
+echo '番組名：'$(echo ${json} | jq '.nowonair_list.g1.present.title' | sed 's/\"//g')
+echo 'サブタイトル：'$(echo ${json} | jq '.nowonair_list.g1.present.subtitle' | sed 's/\"//g')
