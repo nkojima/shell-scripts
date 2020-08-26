264f461b3de53a9734cb210a50be77c8cdf2ea20
kojiro629 <kojiro629@gmail.com>
2020-08-26 22:56:36 +0900

    Add start_time and end_time to 'tv_program.sh'.

--- web-api/tv_program.sh
+++ web-api/tv_program.sh
@@ -7,7 +7,11 @@
 #-------------------------------------------------------------------------------
 json=$(curl -s http://api.nhk.or.jp/v2/pg/now/130/g1.json?key={YOUR_API_KEY})
 
+present_program=$(echo ${json} | jq '.nowonair_list.g1.present')
+
 echo '[現在放送中の番組]'
-echo 'チャンネル：'$(echo ${json} | jq '.nowonair_list.g1.present.service.name' | sed 's/\"//g')
-echo '番組名：'$(echo ${json} | jq '.nowonair_list.g1.present.title' | sed 's/\"//g')
-echo 'サブタイトル：'$(echo ${json} | jq '.nowonair_list.g1.present.subtitle' | sed 's/\"//g')
+echo 'チャンネル：'$(echo ${present_program} | jq '.service.name' | sed 's/\"//g')
+echo '番組名：'$(echo ${present_program} | jq '.title' | sed 's/\"//g')
+echo 'サブタイトル：'$(echo ${present_program} | jq '.subtitle' | sed 's/\"//g')
+echo '開始時刻：'$(echo ${present_program} | jq '.start_time' | sed 's/\"//g')
+echo '終了時刻：'$(echo ${present_program} | jq '.end_time' | sed 's/\"//g')
